0458f74d6ba535b877d6e8abc85f59a513ddd48d
Ignacio Belitzky <ignabelitzky@gmail.com>
2022-09-15 06:29:41 -0300

    Funcionando ejercicio 2a del practico 2

--- arqComputador/practico1/signext.sv
+++ arqComputador/practico1/signext.sv
@@ -3,7 +3,7 @@ module signext(input logic [31:0] a,
 	always_comb begin
 		casez(a[31:21])
 			11'b111_1100_00?0: y = {{55{a[20]}}, a[20:12]};	// LDUR or STUR
-			11'b101_1010_0???: y = {{43{a[23]}}, a[23:5], 2'b00};
+			11'b101_1010_0???: y = {{45{a[23]}}, a[23:5]};	// CBZ
 			default: y = '0;
 		endcase
 	end
